9fc4ffd05d717253b9edb04c5da60b8c455d78b6
mariusz8h@o2.pl <mariusz8h@o2.pl>
2022-11-11 23:45:44 +0100

    cleanup repository

--- KotkiGra/cmake-build-debug/CMakeFiles/3.21.1/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/winlibs-x86_64-posix-seh-gcc-10.3.0-llvm-12.0.0-mingw-w64-9.0.0-r2/mingw64/bin/windres.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .obj)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
